4398e318659f2217a67749b665e2b23e2aae321a
Vedant <vedantgune41@gmail.com>
2021-08-06 12:54:11 +0530

    validity control with dynamic styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,22 +3,20 @@ import Button from "../../UI/Button/Button";
 import styled from "styled-components";
 
 const FormControl = styled.div`
-   {
-    margin: 0.5rem 0;
-  }
+  margin: 0.5rem 0;
 
   & label {
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
-    color : ${(props) => (props.invalid ? "red" : "black")};
+    color: ${(props) => (props.invalid ? "red" : "black")};
   }
 
   & input {
     display: block;
     width: 100%;
     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-    background : ${(props) => (props.invalid ? "#ffd7d7" : "transperant")}
+    background-color: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
